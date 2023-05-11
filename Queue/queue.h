840567a66b713eede8aba0828115ea214b9d4f57
Tucozz <tutuestefanato@gmail.com>
2023-05-11 10:33:18 -0300

    add Queue

---
+++ Queue/queue.h
@@ -0,0 +1,22 @@
+#ifndef _QUEUE_H_
+#define _QUEUE_H_
+
+typedef int data_type;
+typedef struct Queue Queue;
+
+// cria uma queue
+Queue *queue_construct();
+
+// insere um item na queue 
+void queue_enqueue(Queue *queue, data_type value);
+
+// remove o elemento mais antigo da pilha e o retorna
+data_type queue_dequeue(Queue *queue);
+
+// retorna 1 se a queue está vazia e 0 caso contrário
+int queue_empty(Queue *queue);
+
+// libera o espaco alocado para a queue
+void queue_destroy(Queue *queue);
+
+#endif
